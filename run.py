79f80fc185af41ee303c57c1872e5fb46db424e1
Claire Potter <clairepotter019@gmail.com>
2021-07-28 14:17:55 +0000

    Add validation for employee number data

--- run.py
+++ run.py
@@ -7,10 +7,10 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('redeployment_report')
+SHEET = GSPREAD_CLIENT.open("redeployment_report")
 
 
 def get_employee_number():
@@ -20,14 +20,40 @@ def get_employee_number():
     which must be a string of 6 numbers.
     The loop will repeatedly request data, until it is valid.
     """
-    
-    print("Please enter a six digit employee number.")
-    print("The number should not contain any letters or"
-          " special characters.")
-    print("Example: 123456\n")
+    while True:
+        print("Please enter a six digit employee number.")
+        print("The number should not contain any letters or"
+              " special characters.")
+        print("Example: 123456\n")
 
-    employee_number = input("Enter the employee number here:\n")
-    print(employee_number)
+        employee_number = input("Enter the employee number here:\n")
+
+        if validate_number(employee_number):
+            print("Employee Number captured\n")
+            break
+
+    return employee_number
 
 
-get_employee_number()
+def validate_number(value):
+    """
+    Inside the try, converts string value into integer.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        int(value)
+        if len(value) != 6:
+            raise ValueError(
+                f"{len(value)}"
+            )
+    except ValueError as e:
+        print(f"Only a 6 digit number is accepted, you entered {e},"
+              " please try again.\n")
+        return False
+
+    return True
+
+
+emp_number = get_employee_number()
+print(emp_number)
