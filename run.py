770a1d066456cd8c8c8e3b017ed4cb32b3d8f3c3
Claire Potter <clairepotter019@gmail.com>
2021-07-28 14:06:43 +0000

    Add get_employee_number() function

--- run.py
+++ run.py
@@ -12,7 +12,22 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('redeployment_report')
 
-redeployment_pool = SHEET.worksheet('redeployment_pool')
 
-data = redeployment_pool.get_all_values()
-print(data)
+def get_employee_number():
+    """
+    Get a six digit employee number from the user.
+    Run a while loop for user to input data,
+    which must be a string of 6 numbers.
+    The loop will repeatedly request data, until it is valid.
+    """
+    
+    print("Please enter a six digit employee number.")
+    print("The number should not contain any letters or"
+          " special characters.")
+    print("Example: 123456\n")
+
+    employee_number = input("Enter the employee number here:\n")
+    print(employee_number)
+
+
+get_employee_number()
