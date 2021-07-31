14700820f2eded12101863bee2aa96a9dbc4c918
Claire Potter <clairepotter019@gmail.com>
2021-07-31 13:08:37 +0000

    Add corrections and adjustments to code

--- run.py
+++ run.py
@@ -16,24 +16,29 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("redeployment_report")
 
+# The below code is utilised to perform the action of adding a
+# new candidate to the redeployment pool.
+
 
 def get_employee_number():
     """
     Get a six digit employee number from the user.
     Run a while loop for user to input data,
-    which must be a string of 6 numbers.
+    which must be a string of 6 numbers and it cannot be
+    a duplicate employee number.
     The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter a six digit employee number.")
         print("The number should not contain any letters or"
               " special characters.")
+        print("The number should be a unique value.")
         print("Example: 123456\n")
 
         employee_number = input("Enter the employee number here:\n")
 
         if validate_number(employee_number):
-            print("Employee Number captured\n")
+            print("Valid employee number captured.\n")
             break
 
     return employee_number
@@ -43,7 +48,9 @@ def validate_number(value):
     """
     Inside the try, converts string value into integer.
     Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 6 values.
+    or if there aren't exactly 6 values. The second validation checks
+    if the string exists in the employee list from the redeployment pool,
+    if it does the user is notified to enter a unique value.
     """
     try:
         int(value)
@@ -72,12 +79,12 @@ def get_input(name):
     """
     while True:
         print(f"Please enter the {name} of the employee.")
-        print("You cannot enter a number.")
+        print("You cannot enter a number.\n")
 
         input_data = input(f"Enter the {name} here:\n")
 
         if validate_data(input_data):
-            print(f"Valid {name} captured\n")
+            print(f"Valid {name} captured.\n")
             break
 
     return input_data.title()
@@ -85,7 +92,7 @@ def get_input(name):
 
 def validate_data(value):
     """
-    Inside the try, checks of the string is numeric,
+    Inside the try, checks if the string is numeric,
     Raises ValueError if strings is numeric.
     """
     try:
@@ -108,12 +115,12 @@ def get_number(title, number, range, given_range):
     """
     while True:
         print(f"Please enter the {title} of the employee.")
-        print(f"The {title} should be between the range {range}")
+        print(f"The {title} should be between the range {range}.\n")
 
         number = input(f"Enter the {title} here:\n")
 
         if validate_range(number, range, given_range):
-            print(f"Data {title} captured\n")
+            print(f"Valid {title} captured.\n")
             break
 
     return number
@@ -123,7 +130,7 @@ def validate_range(number, range, given_range):
     """
     Inside the try, converts string value into integer.
     Raises ValueError if strings cannot be converted into int,
-    or if value is not between the given range
+    or if value is not between the given range.
     """
     try:
         given_range
@@ -145,7 +152,6 @@ def get_gender():
     Get the gender of the employee from the user.
     Run a while loop for user to select data,
     which must be one of the given values.
-    The loop will repeatedly request data, until it is valid.
     """
     while True:
         gender = [inquirer.List("gender",
@@ -164,19 +170,18 @@ def get_date():
     Get the entry date of the employee into the
     redeployment process from the user.
     Run a while loop for user to input data,
-    which must be a string of letters.
     The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter the date of entry / start date")
         print("of the redeployment process for the employee.")
-        print("Please enter in the format DD/MM/YYYY")
-        print("Example: 01/07/2021 \n")
+        print("Please enter in the format DD/MM/YYYY.")
+        print("Example: 01/07/2021. \n")
 
         date = input("Enter the start date here:\n")
 
         if validate_date(date):
-            print("Date captured\n")
+            print("Valid date captured.\n")
             break
 
     return date
@@ -185,7 +190,7 @@ def get_date():
 def validate_date(my_str_date):
     """
     Raises ValueError if strings cannot be converted into a date and
-    if it is in the incorrect format
+    if it is in the incorrect format.
     """
     try:
         if datetime.strptime(my_str_date, "%d/%m/%Y"):
@@ -204,19 +209,19 @@ def validate_date(my_str_date):
 def update_sheet(data, worksheet):
     """
     Receives a list of values to be inserted into a worksheet
-    Update the relevant worksheet with the data provided
+    Updates the relevant worksheet with the data provided.
     """
     print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated successfully\n")
+    print(f"{worksheet} worksheet updated successfully.\n")
 
 
 def add_candidate():
     """
     Run all program functions to add a candidate to the
     Redeployment Process and save the data to the redeployment pool
-    worksheet
+    worksheet.
     """
     print("Please proceed to add a new employee.\n")
     age_range = range(18, 76, 1)
@@ -239,9 +244,14 @@ def add_candidate():
     employee = [int(emp_number), emp_name, emp_surname, int(emp_age),
                 emp_gender, emp_department, emp_position, int(emp_salary),
                 int(emp_years), int(emp_months),
-                emp_date, " ", " ", "Active"]
-
+                " ", " ", " ", "Active"]
     update_sheet(employee, "redeployment_pool")
+    sheet = SHEET.worksheet("redeployment_pool")
+    cell = sheet.find(emp_number)
+    row_no = "%s" % (cell.row)
+    cell_2 = sheet.find("Entry Date")
+    col_no = "%s" % (cell_2.col)
+    sheet.update_cell(row_no, col_no, emp_date)
     main()
 
 
@@ -430,10 +440,11 @@ def update_candidate():
     """
     update_process()
     answer = update_another_field()
-    if answer == "Yes":
+    while answer == "Yes":
         update_process()
         update_another_field()
-    elif answer == "No":
+        break
+    if answer == "No":
         main()
 
 
